826a8f69394d586842321ac3cef465481bdfe113
Арам Григорян <a.grigoryan@energochain.ru>
2022-10-02 15:36:49 +0300

    small refactor

--- client/src/http/index.js
+++ client/src/http/index.js
@@ -1,23 +1,21 @@
-import axios from "axios";
-
-const AUTH_TOKEN = '';
+import axios from 'axios';
 
 const $host = axios.create({
     baseURL: 'http://localhost:8081/'
-})
+});
 
 const $authHost = axios.create({
     baseURL: 'https://localhost:8081/'
-})
+});
 
 const authInterceptor = config => {
-    config.headers.authorization = `Bearer ${localStorage.getItem('token')}`
-    return config
-}
+    config.headers.authorization = `Bearer ${localStorage.getItem('token')}`;
+    return config;
+};
 
-$authHost.interceptors.request.use(authInterceptor)
+$authHost.interceptors.request.use(authInterceptor);
 
 export {
     $host,
     $authHost
-}
+};
